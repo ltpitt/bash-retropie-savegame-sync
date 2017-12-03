daf7dc5f10e72c512157fc34ab608861e057b706
Davide Nastri <d.nastri@gmail.com>
2017-12-03 19:31:37 +0100

    feat: Optimized script logic

--- retropie-savegame-sync.sh
+++ retropie-savegame-sync.sh
@@ -1,9 +1,9 @@
+# RetroPie Savegame SyncThingie by @pitto
 # https://github.com/ltpitt
 #
-# This is a very quick and sloppy script to keep savegames
-# in sync between two RetroPie devices
+# This script keeps to keep savegames in sync between my two
+# RetroPie devices: a Raspberry Pi 3 and a Gameboy Zero
 #
-# I've tested it using a Raspberry Pi 3 and a Gameboy Zero
 # You can launch it manually or add it to cron or /etc/rc.local
 #
 # If you plan to execute it automatically you have to  be sure 
@@ -11,27 +11,29 @@
 # using, for example, ssh-copy-id
 
 # Customize the following variables according to your setup
-SOURCE_1="pi@retropie:/home/pi/RetroPie/roms/"
-SOURCE_2="pi@gameboyzero:/home/pi/RetroPie/roms/"
-TARGET="/your/target/folder/"
-BACKUP="/home/pi/backup_savegames/"
+LOCAL_ROM_FOLDER="/home/pi/RetroPie/roms/"
+REMOTE_ROM_FOLDER="pi@gameboyzero:/home/pi/RetroPie/roms/"
+BACKUP="/home/pi/RetroPie/backup_savegames/"
 
 # There should be no need to change anything below this line
 clear
+# With set -e the script will quit in case of any error, better safe than sorry
+set -e
+# In the now variable I put a timestamp that will be later used in backup folder name
 NOW=`date +"%Y%m%d%H%M"`
 echo "RetroPie Savegame SyncThingie"
 echo
-echo "Creating SOURCE_1 and SOURCE_2 backup folder into: $BACKUP$NOW..."
-mkdir -p $BACKUP$NOW/SOURCE_1
-mkdir -p $BACKUP$NOW/SOURCE_2
+echo "Creating LOCAL_SAVEGAMES and REMOTE_SAVEGAMES folders into: $BACKUP$NOW..."
+mkdir -p $BACKUP$NOW/local_savegames
+mkdir -p $BACKUP$NOW/remote_savegames
 echo
-echo "Backing up $SOURCE_1 savegames into: $BACKUP$NOW/SOURCE_1..."
-rsync -avzp --include '*/' --include '*.srm' --exclude '*' $SOURCE_1 $BACKUP$NOW/SOURCE_1
+echo "Backing up $LOCAL_ROM_FOLDER savegames into: $BACKUP$NOW/LOCAL_SAVEGAMES..."
+rsync -avzp --include '*/' --include '*.srm' --exclude '*' $LOCAL_ROM_FOLDER $BACKUP$NOW/local_savegames
 echo
-echo "Backing up $SOURCE_1 savegames into: $BACKUP$NOW/SOURCE_1..."
-rsync -avzp --include '*/' --include '*.srm' --exclude '*' $SOURCE_2 $BACKUP$NOW/SOURCE_2
+echo "Backing up $REMOTE_ROM_FOLDER savegames into: $BACKUP$NOW/REMOTE_SAVEGAMES..."
+rsync -avzp --include '*/' --include '*.srm' --exclude '*' $REMOTE_ROM_FOLDER $BACKUP$NOW/remote_savegames
 echo
-echo "Updating to newest version all savegames in $TARGET"
-rsync -avzp --include '*/' --include '*.srm' --exclude '*' $SOURCE_1 $TARGET
-rsync -avzp --include '*/' --include '*.srm' --exclude '*' $SOURCE_2 $TARGET
+echo "Updating to newest version savegames in $LOCAL_ROM_FOLDER and $REMOTE_ROM_FOLDER"
+rsync -avzp --include '*/' --include '*.srm' --exclude '*' $LOCAL_ROM_FOLDER $REMOTE_ROM_FOLDER
+rsync -avzp --include '*/' --include '*.srm' --exclude '*' $REMOTE_ROM_FOLDER $LOCAL_ROM_FOLDER
 echo "Done! :)"
